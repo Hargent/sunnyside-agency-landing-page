1973cceb373e53d3fcb52a4f38339cfc19f1b78d
Hargent <yemisi3385@gmail.com>
2023-07-23 19:19:57 +0100

    mobile nav completed, final styling left

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-js
+const headerMenu = document.querySelector(".header__menu");
+
+const handleModalEvent = e => {
+	const btn = e.target.closest(".modal-btn");
+	if (!btn) return;
+	const mobileNav = headerMenu.querySelector(".mobile__nav");
+
+	mobileNav.classList.toggle("toggle-display");
+	// console.log(mobileNav);
+};
+headerMenu.addEventListener("click", handleModalEvent);
+// console.log(headerMenu);
+
+// const width = window.innerWidth;
+// if (width === 800) {
+// 	console.log("yes");
+// }
+// console.log(width);
